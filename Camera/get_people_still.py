e6e31890569cffa7298af3b6e4e3928f2f1bd0c3
Jamie-UoG <2137284s@student.gla.ac.uk>
2017-12-07 22:07:48 +0000

    Fixed image recognition script, can not parse object results

--- Camera/get_people_still.py
+++ Camera/get_people_still.py
@@ -7,9 +7,8 @@ DN_DIR = "/home/pi/darknet-nnpack/"
 DATA_FILE = DN_DIR + "cfg/voc.data"
 CFG_FILE = DN_DIR + "cfg/tiny-yolo-voc.cfg"
 WEIGHTS_FILE = DN_DIR + "tiny-yolo-voc.weights"
-'''
-camera = picamera.PiCamera()
 
+camera = picamera.PiCamera()
 camera.sharpness = 0
 camera.contrast = 0
 camera.brightness = 50
@@ -26,12 +25,21 @@ camera.rotation = 0
 camera.hflip = True
 camera.vflip = True
 camera.crop = (0.0, 0.0, 1.0, 1.0)
-'''
 
-
-#while (True):
-#camera.capture( DN_DIR + "testing.jpg")
+camera.capture( DN_DIR + "testing.jpg")
 call_str = "./darknet detector test " + DATA_FILE +" "+ CFG_FILE +" "+ WEIGHTS_FILE + " testing.jpg"
-a = subprocess.Popen(call_str, cwd=DN_DIR, shell=True, stderr=subprocess.PIPE)
-#    print a.communicate()
- #   time.sleep(2)
+a = subprocess.Popen(call_str, cwd=DN_DIR, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out = a.communicate()
+obj_list = out[0].split('\n')[1:-1] 
+person_list = []
+
+for obj in obj_list:
+    obj_split = obj.split(': ')
+    if obj_split[0] == "person":
+        person_list += [obj]
+
+print obj_list
+print person_list
+print str(len(person_list))
+
+        
