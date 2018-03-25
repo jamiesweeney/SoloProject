0536ea9b7908130588f48decf92698489dbe1689
Jamie-UoG <2137284s@student.gla.ac.uk>
2018-03-25 17:52:42 +0000

    local changes

--- src/test/bluetoothLEScannerUnitTests.py
+++ src/test/bluetoothLEScannerUnitTests.py
@@ -14,35 +14,35 @@ class TestAddressHashing(unittest.TestCase):
 
     def test_null(self):
         address = None
-        self.assertTrue(btle.hash_addrs(address) == None)
+        self.assertTrue(btle.hash_address(address) == None)
 
     def test_nvalid(self):
         address = "not a mac address"
-        self.assertTrue(btle.hash_addrs(address) == None)
+        self.assertTrue(btle.hash_address(address) == None)
 
         address = "1234567890"
-        self.assertTrue(btle.hash_addrs(address) == None)
+        self.assertTrue(btle.hash_address(address) == None)
 
         address = ""
-        self.assertTrue(btle.hash_addrs(address) == None)
+        self.assertTrue(btle.hash_address(address) == None)
 
         address = "yy:yy:yy:yy:yy"
-        self.assertTrue(btle.hash_addrs(address) == None)
+        self.assertTrue(btle.hash_address(address) == None)
 
     def test_valid(self):
         address = "30:10:B3:21:2D:3E"
-        ans1 = btle.hash_addrs(address)
+        ans1 = btle.hash_address(address)
         self.assertTrue(ans1 != None)
         self.assertTrue(ans1 != address)
         self.assertTrue(len(ans1) == 28)
-        self.assertTrue(ans1 == btle.hash_addrs(address))
+        self.assertTrue(ans1 == btle.hash_address(address))
 
         address = "3010B3212D3E"
-        ans2 = btle.hash_addrs(address)
+        ans2 = btle.hash_address(address)
         self.assertTrue(ans2 != None)
         self.assertTrue(ans2 != address)
         self.assertTrue(len(ans2) == 28)
-        self.assertTrue(ans2 == btle.hash_addrs(address))
+        self.assertTrue(ans2 == btle.hash_address(address))
 
         self.assertTrue(ans1 == ans2)
 
